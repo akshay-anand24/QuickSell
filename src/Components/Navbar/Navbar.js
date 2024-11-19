171296dccf011a6f32c57c909cd2453afec776eb
Akshay Anand <21bcs015@iiitdmj.ac.in>
2024-11-20 02:04:35 +0530

    bugs fixed in Navbar and build

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import filterIcon from '../../Assets/Images/Tuning.svg'
-import downIcon from '../../Assets/Images/Down.svg'
+// import filterIcon from '../../Assets/Images/Tuning.svg'
+// import downIcon from '../../Assets/Images/Down.svg'
 
 import './style.css'
 
